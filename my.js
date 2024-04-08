6607a1563c6b9a77ce9feca363f59feacb1ac6a1
Abdul Haadi <haadialiraj2003@gmail.com>
2024-04-09 02:05:13 +0500

    Initial commit

---
+++ my.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your form submission logic here
+    alert('Your message has been sent!');
+  });
+  
